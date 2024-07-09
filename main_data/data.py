01dd2d47e31a08edf2bd2ecc963595e1382096f5
jollyreap <leonid1988mail@mail.ru>
2024-07-09 15:57:45 +0300

    evm balance checker v1 data needed to process web3 reqs added evm chains in setting.py

---
+++ main_data/data.py
@@ -0,0 +1,202 @@
+DATA = {
+    "ethereum": {
+        "rpc": "https://rpc.ankr.com/eth",
+        "scan": "https://etherscan.io/tx",
+        "token": "ETH",
+        "chain_id": 1
+    },
+    "optimism": {
+        "rpc": "https://rpc.ankr.com/optimism",
+        "scan": "https://optimistic.etherscan.io/tx",
+        "token": "ETH",
+        "chain_id": 10
+    },
+    "bsc": {
+        "rpc": "https://rpc.ankr.com/bsc",
+        "scan": "https://bscscan.com/tx",
+        "token": "BNB",
+        "chain_id": 56
+    },
+    "polygon": {
+        "rpc": "https://rpc.ankr.com/polygon",
+        "scan": "https://polygonscan.com/tx",
+        "token": "MATIC",
+        "chain_id": 137
+    },
+    "polygon_zkevm": {
+        "rpc": "https://zkevm-rpc.com",
+        "scan": "https://zkevm.polygonscan.com/tx",
+        "token": "ETH",
+        "chain_id": 1101
+    },
+    "arbitrum": {
+        "rpc": "https://rpc.ankr.com/arbitrum",
+        "scan": "https://arbiscan.io/tx",
+        "token": "ETH",
+        "chain_id": 42161
+    },
+    "avalanche": {
+        "rpc": "https://rpc.ankr.com/avalanche",
+        "scan": "https://snowtrace.io/tx",
+        "token": "AVAX",
+        "chain_id": 43114
+    },
+    "fantom": {
+        "rpc": "https://rpc.ankr.com/fantom",
+        "scan": "https://ftmscan.com/tx",
+        "token": "FTM",
+        "chain_id": 250
+    },
+    "nova": {
+        "rpc": "https://nova.arbitrum.io/rpc",
+        "scan": "https://nova.arbiscan.io/tx",
+        "token": "ETH",
+        "chain_id": 42170
+    },
+    "zksync": {
+        "rpc": "https://mainnet.era.zksync.io",
+        "scan": "https://explorer.zksync.io/tx",
+        "token": "ETH",
+        "chain_id": 324
+    },
+    "celo": {
+        "rpc": "https://1rpc.io/celo",
+        "scan": "https://celoscan.io/tx",
+        "token": "CELO",
+        "chain_id": 42220
+    },
+    "gnosis": {
+        "rpc": "https://rpc.ankr.com/gnosis",
+        "scan": "https://gnosisscan.io/tx",
+        "token": "xDAI",
+        "chain_id": 100
+    },
+    "core": {
+        "rpc": "https://rpc.coredao.org",
+        "scan": "https://scan.coredao.org/tx",
+        "token": "CORE",
+        "chain_id": 1116
+    },
+    "harmony": {
+        "rpc": "https://api.harmony.one",
+        "scan": "https://explorer.harmony.one/tx",
+        "token": "ONE",
+        "chain_id": 1666600000
+    },
+    "moonbeam": {
+        "rpc": "https://rpc.ankr.com/moonbeam",
+        "scan": "https://moonscan.io/tx",
+        "token": "GLMR",
+        "chain_id": 1284
+    },
+    "moonriver": {
+        "rpc": "https://moonriver.public.blastapi.io",
+        "scan": "https://moonriver.moonscan.io/tx",
+        "token": "MOVR",
+        "chain_id": 1285
+    },
+    "linea": {
+        "rpc": "https://rpc.linea.build",
+        "scan": "https://lineascan.build/tx",
+        "token": "ETH",
+        "chain_id": 59144
+    },
+    "base": {
+        "rpc": "https://mainnet.base.org",
+        "scan": "https://basescan.org/tx",
+        "token": "ETH",
+        "chain_id": 8453
+    },
+    "zora": {
+        "rpc": "https://zora.rpc.thirdweb.com",
+        "scan": "https://zora.superscan.network/tx",
+        "token": "ETH",
+        "chain_id": 7777777
+    },
+    "scroll": {
+        "rpc": "https://scroll.blockpi.network/v1/rpc/public",
+        "scan": "https://scrollscan.com/tx",
+        "token": "ETH",
+        "chain_id": 534352
+    },
+    "metis": {
+        "rpc": "https://metis-mainnet.public.blastapi.io",
+        "scan": "https://andromeda-explorer.metis.io/tx",
+        "token": "METIS",
+        "chain_id": 1088
+    },
+    "canto": {
+        "rpc": "https://canto.slingshot.finance",
+        "scan": "https://cantoscan.com/tx",
+        "token": "CANTO",
+        "chain_id": 7700
+    },
+    "starknet": {
+        "rpc": "https://starknet-mainnet.public.blastapi.io",
+        "scan": "https://starkscan.co/tx",
+        "token": "ETH",
+        "chain_id": None
+    },
+    "mantle": {
+        "rpc": "https://1rpc.io/mantle",
+        "scan": "https://explorer.mantle.xyz/tx",
+        "token": "MNT",
+        "chain_id": 5000
+    },
+    "blast": {
+        "rpc": "https://blast.blockpi.network/v1/rpc/public",
+        "scan": "https://blastscan.io/tx",
+        "token": "ETH",
+        "chain_id": 81457
+    },
+    "zeta": {
+        "rpc": "https://zetachain-evm.blockpi.network/v1/rpc/public",
+        "scan": "https://explorer.zetachain.com",
+        "token": "ZETA",
+        "chain_id": 7000
+    },
+    "mode": {
+        "rpc": "https://1rpc.io/mode",
+        "scan": "https://explorer.mode.network/tx",
+        "token": "ETH",
+        "chain_id": 34443
+    },
+    "opbnb": {
+        "rpc": "https://opbnb-rpc.publicnode.com",
+        "scan": "https://opbnb.bscscan.com/tx",
+        "token": "BNB",
+        "chain_id": 204
+    },
+}
+
+MULTICALL_ETH_CONTRACTS = {
+    'ethereum': '0xb1f8e55c7f64d203c1400b9d8555d050f94adf39',
+    'arbitrum': '0x151E24A486D7258dd7C33Fb67E4bB01919B7B32c',
+    'bsc': '0x2352c63A83f9Fd126af8676146721Fa00924d7e4',
+    'polygon': '0x2352c63A83f9Fd126af8676146721Fa00924d7e4',
+    'optimism': '0xB1c568e9C3E6bdaf755A60c7418C269eb11524FC',
+    'avalanche': '0xD023D153a0DFa485130ECFdE2FAA7e612EF94818',
+    'fantom': '0x07f697424ABe762bB808c109860c04eA488ff92B',
+    'era': '0x875fb0451fb2787b1924edc1DE4083E5f63D99Df',
+    'zksync': '0x875fb0451fb2787b1924edc1DE4083E5f63D99Df',
+    'nova': '0x3008e6ad64a470c47f428e73214c2f1f4e79b72d',
+    'zora': '0x6830d287fE1dab06ABe252911caD71F37a0514A3',
+    'linea': '0x3008e6ad64a470c47f428e73214C2F1f4e79b72d',
+    'base': '0x162708433f00dbc8624795f181ec1983e418ef11',
+    'polygon_zkevm': '0x162708433F00DBC8624795F181EC1983E418EF11',
+    'core': '0xdAd633A2Ff9fb3Ab5d7a8bcfd089593c503c11a2',
+    'gnosis': '0xd08149E71671A284e3F99b371BaF29BB8eEA7387',
+    'goerli': '0x8242cd33761782f02bf10b7329cea5faf17b2bea',
+    'moonbeam': '0xf614056a46e293DD701B9eCeBa5df56B354b75f9',
+    'moonriver': '0xDEAa846cca7FEc9e76C8e4D56A55A75bb0973888',
+    'aurora': '0x100665685d533F65bdD0BD1d65ca6387FC4F4FDB',
+    'tron': 'TN8RtFXeQZyFHGmH1iiSRm5r4CRz1yWkCf',
+    'celo': '0x6830d287fE1dab06ABe252911caD71F37a0514A3',
+    'harmony': '0x3008e6ad64a470c47f428e73214c2f1f4e79b72d',
+    'scroll': '0xD44a774539062bB4dC032df336595161c8a0CC52',
+    'mantle': '0x3439cD286f92713db1d60Cf183597689eaF19117',
+    'blast': '0x7ca5f6398cb59a64b4ebe61181c6045c2f15064a',
+    'mode': '0xEbc45eBC6B4f74b5411771900999c51c98E5CccA',
+    'zora': '0x41CD56d7A899bF9c284ca04DCb16bE0B65c74917',
+    'opbnb': '0x72b3797e1edbeefcb3d8ac720b15b078d797d547',
+}
--- modules/checker.py
+++ modules/checker.py
@@ -1,19 +1,165 @@
+import math
+import asyncio
+import aiohttp
 
-class EvmBalanceChecker: # structure of checker
-    def __init__(self):
-        ...
+from web3 import Web3, AsyncHTTPProvider
+from web3.eth import AsyncEth
 
-    def get_web3(self, chain):
-        ...
+from setting import Value_EVM_Balance_Checker
+from main_data.data import DATA
+from modules.utils.loader import load_erc20
+from main_data.data import MULTICALL_ETH_CONTRACTS
+
+
+class EvmBalanceChecker:
+    # def __init__(self, list_pks: list):
+    #     self.pks = list_pks
+
+    @staticmethod
+    def get_web3(chain):
+        return Web3(
+            provider=Web3.AsyncHTTPProvider(
+                endpoint_uri=DATA[chain]['rpc'],
+            ),
+            modules={'eth': (AsyncEth,)},
+            middlewares=[])
 
     async def get_tokens_data(self):
-        ...
+        list_symbols = {}
+        for chain, coins in Value_EVM_Balance_Checker.evm_tokens.items():
+            web3 = EvmBalanceChecker.get_web3(chain)
+            list_symbols[chain] = {}
+            for coin in coins:
+                if coin != '':
+                    coin = Web3.to_checksum_address(coin)
+                    token_contract = web3.eth.contract(address=coin, abi=load_erc20())
+                    try:
+                        symbol = await token_contract.functions.symbol().call()
+                        list_symbols[chain][coin] = symbol
+                    except Exception as e:
+                        print(f"Error fetching symbol for {coin}: {e}")
+
+        return list_symbols
 
     async def fetch_price(self, session, symbol, url):
-        ...
+        try:
+            async with session.get(url) as response:
+                result = await response.json()
+                price = result['USDT']
+                self.prices[symbol] = float(price)
+        except Exception as error:
+            self.prices[symbol] = 0
 
     async def get_prices(self):
-        ...
+        self.prices = {}
+        for chain, coins in Value_EVM_Balance_Checker.evm_tokens.items():
+            web3 = self.get_web3(chain)
+            for address_contract in coins:
+                if address_contract == '':  # eth
+                    symbol = DATA[chain]['token']
+                else:
+                    token_contract = web3.eth.contract(address=Web3.to_checksum_address(address_contract),
+                                                       abi=load_erc20())
+                    symbol = await token_contract.functions.symbol().call()
 
-    async def evm_balances(self):
-        ...
+                self.prices.update({symbol: 0})
+
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for symbol in self.prices:
+                if symbol == 'CORE':
+                    url = f'https://min-api.cryptocompare.com/data/price?fsym=COREDAO&tsyms=USDT'
+                else:
+                    url = f'https://min-api.cryptocompare.com/data/price?fsym={symbol}&tsyms=USDT'
+
+                tasks.append(self.fetch_price(session, symbol, url))
+
+            await asyncio.gather(*tasks)
+
+    async def get_balances(self, chain, wallets, tokens_list, symbols_list):
+
+        if "" in tokens_list:
+            index = tokens_list.index("")  # Находим индекс первого вхождения элемента
+            tokens_list[index] = "0x0000000000000000000000000000000000000000"
+
+        wallets_ = [Web3.to_checksum_address(wallet) for wallet in wallets]
+        tokens_list = [Web3.to_checksum_address(token) for token in tokens_list]
+
+        batch_limit = 1000
+        print(chain)
+        len_batch = math.floor(batch_limit / len(tokens_list))
+        wallets_batches = [wallets_[i:i + len_batch] for i in range(0, len(wallets_), len_batch)]
+
+        balances_dict = {}
+        zero = 0
+        for wallets_list in wallets_batches:
+            max_attempts = 10
+            for attempt in range(max_attempts):
+                try:
+                    web3 = EvmBalanceChecker.get_web3(chain)
+                    abi = '[{"constant":true,"inputs":[{"name":"user","type":"address"},{"name":"token","type":"address"}],"name":"tokenBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"users","type":"address[]"},{"name":"tokens","type":"address[]"}],"name":"balances","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"}]'
+                    multicall_contract = web3.eth.contract(
+                        address=Web3.to_checksum_address(MULTICALL_ETH_CONTRACTS[chain]), abi=abi)
+                    multicall_result = await multicall_contract.functions.balances(
+                        wallets_list, tokens_list
+                    ).call()
+
+                    zero += len(wallets_list)
+                    multicall_result = [multicall_result[i:i + len(tokens_list)] for i in
+                                        range(0, len(multicall_result), len(tokens_list))]
+
+                    for number, wallet in enumerate(wallets_list):
+                        balances = multicall_result[number]
+                        balances_dict[wallet] = {}
+
+                        for i, balance in enumerate(balances):
+                            token = tokens_list[i]
+                            if token == '0x0000000000000000000000000000000000000000':
+                                symbol = DATA[chain]['token']
+                            else:
+                                symbol = symbols_list[chain][token]
+
+                            balances_dict[wallet][symbol] = int(balance / 10 ** 6)
+                    break
+
+                except Exception as e:
+                    await asyncio.sleep(1)
+        print(balances_dict)
+        return balances_dict
+
+    async def evm_balances(self, tokens_data):
+
+        wallets = [
+            '0xec3d68dc25be091b812a67b075f4668e4e4183c3',
+            '0x9d17bb55b57b31329cf01aa7017948e398b277bc',
+            '0x0edefa91e99da1eddd1372c1743a63b1595fc413',
+            '0xbdfa4f4492dd7b7cf211209c4791af8d52bf5c50',
+            '0x41bc7d0687e6cea57fa26da78379dfdc5627c56d',
+            '0x6cd68e8f04490cd1a5a21cc97cc8bc15b47dc9eb',
+            '0x192820ce84fa9eb457fb228c386fe0ed22f7e33c',
+            '0x0172e05392aba65366c4dbbb70d958bbf43304e4'
+        ]
+        chains = DATA.keys()
+        # tokens_data = await self.get_tokens_data()
+        tasks = []
+
+        for chain in chains:
+            if chain in tokens_data:
+                tokens_list = tokens_data[chain].keys()
+                symbols_list = tokens_data[chain].values()
+
+                task = asyncio.create_task(self.get_balances(chain, wallets, tokens_list, symbols_list))
+                tasks.append(task)
+
+        results = await asyncio.gather(*tasks)
+
+        return results
+
+
+async def main():
+    evm = EvmBalanceChecker()
+    td = await evm.get_tokens_data()
+    print(await evm.evm_balances(td))
+
+if __name__ == "__main__":
+    asyncio.run(main())
