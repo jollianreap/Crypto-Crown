8d46abaec3fea95e8a54808ced280d318f3ec531
onttix <dvudgexn@gmail.com>
2024-07-11 13:48:12 +0300

    solved errors

--- modules/arb.py
+++ modules/arb.py
@@ -32,20 +32,20 @@ class AutoWithdraw:
         assert await AutoWithdraw.check_rpc(w3) is True, "rpc doesn't work"
         return w3
 
-    async def get_sender(self, w3: Web3):
-        account = await w3.eth.account.from_key(self.private_key)
+    def get_sender(self, w3: Web3):
+        account = w3.eth.account.from_key(self.private_key)
         address = account.address
         return address
 
     async def create_tx(self, chain_id, w3: Web3):
-        balance = await w3.eth.get_balance(await self.get_sender(w3))
+        balance = await w3.eth.get_balance(self.get_sender(w3))
         default_gas_price = await w3.eth.gas_price
 
         transaction = {
             'to': self.receiver_address,
             'value': balance,
             'gasPrice': default_gas_price,
-            'nonce': w3.eth.get_transaction_count(await self.get_sender(w3)),
+            'nonce': await w3.eth.get_transaction_count(self.get_sender(w3)),
             'chainId': chain_id
         }
 
@@ -62,9 +62,9 @@ class AutoWithdraw:
     async def send_tx(self, net_name):
         chain_id, rpc = DATA[net_name]['chain_id'], DATA[net_name]['rpc']
         print(f'Process tx in {net_name} net ({chain_id}) via this rpc: {rpc}')
-        w3 = AutoWithdraw.get_w3(rpc)
-        tx = self.create_tx(chain_id)
-        signed_txn = await w3.eth.account.sign_transaction(tx, self.private_key)
+        w3 = await AutoWithdraw.get_w3(rpc)
+        tx = await self.create_tx(chain_id, w3)
+        signed_txn = w3.eth.account.sign_transaction(tx, self.private_key)
         txn_hash = await w3.eth.send_raw_transaction(signed_txn.rawTransaction)
         txn_receipt = await w3.eth.wait_for_transaction_receipt(txn_hash)
         return f'Transaction in {net_name} net is approved: {txn_receipt}'
@@ -76,5 +76,5 @@ class AutoWithdraw:
             task = asyncio.create_task(self.send_tx(net_name))
             tasks.append(task)
 
-        results = asyncio.gather(*tasks)
+        results = await asyncio.gather(*tasks)
         return results
